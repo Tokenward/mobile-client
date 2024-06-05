00297e1db5bfa1658bf404770ea3a5256b942430
Mitja Kurath <mitja.kurath@bbcag.ch>
2024-06-05 11:11:41 +0200

    [UPDATE] Added Firebase config

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
+ 
+// firebase config
+const firebaseConfig = {
+    apiKey: Constants.expoConfig.extra.apiKey,
+    authDomain: Constants.expoConfig.extra.authDomain,
+    projectId: Constants.expoConfig.extra.projectId,
+    storageBucket: Constants.expoConfig.extra.storageBucket,
+    messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+    appId: Constants.expoConfig.extra.appId,
+    measurementId: Constants.expoConfig.extra.measurementId
+};
+ 
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
