a72aa43f9e751c0250c8780d8315b0f67676e320
Nxkzty <leon.hoefferer@gmail.com>
2024-06-05 13:17:47 +0200

    [ADD] Added SignUp page

--- app/pages/register/index.js
+++ app/pages/register/index.js
@@ -13,4 +13,32 @@ export default function RegisterScreen() {
           console.error(error);
         }
     };
+
+    return (
+      <SafeAreaView style={Styles.mainContainer}>
+          <StatusBar backgroundColor="#393939" barStyle="light-content" />
+          <View style={Styles.mainContainer}>
+              <Text style={Styles.title}>SignUp</Text>
+              <InputBox label={"Email"} value={email} onChangeText={setEmail}></InputBox>
+              <InputBox label={"Password"} hidden={true} value={password} onChangeText={setPassword}></InputBox>
+              <CustomButton onPress={handleSignUp}>SignUp</CustomButton>
+          </View>
+
+      </SafeAreaView>
+  )
 }
+const Styles = StyleSheet.create({
+  mainContainer: {
+      backgroundColor:"#393939",
+      height: "100%",
+  },
+  title: {
+      fontSize: 30,
+      color: "white",
+      textAlign: "center",
+      marginTop: 48,
+      fontWeight: "bold",
+      fontFamily: "roboto",
+  },
+
+})
