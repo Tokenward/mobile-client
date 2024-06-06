d7a8e82cee14c970fb65cd007f461fd1a7847cc7
Leon Höfferer <leon.hoefferer@gmail.com>
2024-06-06 20:57:11 +0200

    [UPDATE] Added new function in /lib/api/item.js for item saving/loading stuff

--- components/CreateNewItem.js
+++ components/CreateNewItem.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
-import { auth, database } from '../config/firebase';
 import CustomButton from './CustomButton';
 import { useRouter } from "expo-router";
 import InputBox from './InputBox';
-import { collection, addDoc } from 'firebase/firestore';
+import saveNewItem from '../lib/api/item';
 
 export default function CreateNewItem({ navigation }) {
     const [type, setType] = useState('noFolderItems');
@@ -14,23 +13,8 @@ export default function CreateNewItem({ navigation }) {
     const [icon, setIcon] = useState('');
     const router = useRouter();
 
-    const handleSave = async () => {
-        console.log("Saving new Item...");
-        const user = auth.currentUser;
-        console.log("Starting if statement...");
-        if (user && title && type) {
-            console.log("If statement passed");
-            const userId = user.uid;
-            const newItem = { label, icon, content };
-
-            try {
-                await addDoc(collection(database, 'users', userId, type), newItem);
-                console.log("Document successfully written!");
-                router.navigate("../../(tabs)/vault");
-            } catch (error) {
-                console.error("Error adding document: ", error);
-            }
-        }
+    const handleSave = () => {
+        saveNewItem(title, type, label, icon, content, router);
     };
 
     return (
