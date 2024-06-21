2a86fe3cd872fcd8367ffe8cab444a4dcc9de7c0
nxkzty <Leon.hoefferer@gmail.com>
2024-06-21 11:29:15 +0200

    added logout button oniichahannann

--- app/pages/(tabs)/settings/index.js
+++ app/pages/(tabs)/settings/index.js
@@ -4,6 +4,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import InputBox from '../../../../components/essential/InputBox';
 import CustomButton from '../../../../components/essential/CustomButton';
 import useThemeContext from '../../../../lib/hooks/useThemeContext';
+import { EmailAuthProvider, reauthenticateWithCredential, updatePassword } from 'firebase/auth';
+import { auth } from '../../../../config/firebase';
+import { Alert } from 'react-native';
+import logout from '../../../index' 
+import { useRouter } from 'expo-router';
+import { getAuth, signOut } from 'firebase/auth';
+
 
 export default function SettingsScreen() {
     const [oldPassword, setOldPassword] = useState('');
@@ -12,6 +19,54 @@ export default function SettingsScreen() {
     const [newEmail, setNewEmail] = useState('');
 
     const colors = useThemeContext();
+
+    const auth = getAuth();
+    const router = useRouter();
+    
+    const logoutUser = () => {
+        signOut(auth)
+           .then(() => {
+                router.push('/auth')
+                logout();
+            })
+           .catch((error) => {
+                console.error('Sign out error:', error);
+                Alert.alert('Error', 'An error occurred while signing out. Please try again later.');
+            });
+    }
+
+
+
+    const handleChangePassword = () => {
+        if (oldPassword !== newPassword) {
+            if (newPassword === confirmPassword) {
+                const user = auth.currentUser;
+                const credential = EmailAuthProvider.credential(user.email, oldPassword);
+
+                reauthenticateWithCredential(user, credential)
+                    .then(() => {
+                        updatePassword(user, newPassword)
+                            .then(() => {
+                                console.log('Password updated successfully!');
+                                Alert.alert('Success', 'Your password has been updated successfully.');
+                            })
+                            .catch((error) => {
+                                console.error('Password update error:', error);
+                                Alert.alert('Error', 'An error occurred while updating your password. Please try again later.');
+                            });
+                    })
+                    .catch((error) => {
+                        console.error('Reauthentication error:', error);
+                        Alert.alert('Error', 'Your old password is incorrect. Please try again.');
+                    });
+            } else {
+                Alert.alert('Error', 'Your new password and confirm password do not match. Please try again.');
+            }
+        } else {
+            Alert.alert('Error', 'Your new password cannot be the same as your old password. Please try again.');
+        }
+    };
+
 
     return (
         <SafeAreaView style={[styles.safeArea, { backgroundColor: colors.background }]}>
@@ -22,12 +77,16 @@ export default function SettingsScreen() {
                     <InputBox label="Current Password" value={oldPassword} hidden={true} onChangeText={setOldPassword} />
                     <InputBox label="New Password" value={newPassword} hidden={true} onChangeText={setNewPassword} />
                     <InputBox label="Confirm Password" value={confirmPassword} hidden={true} onChangeText={setConfirmPassword} />
-                    <CustomButton>Confirm new password</CustomButton>
+                    <CustomButton onPress={handleChangePassword}>Confirm new password</CustomButton>
                 </View>
                 <View style={[styles.settingContainer, { backgroundColor: colors.surface }]}>
                     <Text style={[styles.headerText, { color: colors.onSurface }]}>Change Email</Text>
                     <InputBox label="Current Password" value={newEmail} hidden={false} onChangeText={setNewEmail} />
                     <CustomButton>Confirm new Email</CustomButton>
+                </View>
+                <View style={[styles.settingContainer, { backgroundColor: colors.surface }]}>
+
+                    <CustomButton onPress={logout}>Logout</CustomButton>
                 </View>
             </View>
 
